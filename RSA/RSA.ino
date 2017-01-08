338ff980dc5d21ce2c77efbe724742beedfaa9a4
gcehulic <goran.cehulic@gmail.com>
2017-01-08 12:55:10 +0100

    RSA kriptiranje teksta
    Program se izvršava na NodeMCU ESP8266. Svi parametri su zadani u kodu.
    Radi se kriptiranje i dekriptiranje teksta.

---
+++ RSA/RSA.ino
@@ -0,0 +1,97 @@
+// RSA_kriptiranje i dekriptiranje
+
+#include <Time.h>
+
+//Kriptiranje
+int encrypt(int text, int n, int e){
+   int sifrat=1;
+   for(int i=0; i<e; i++){
+    sifrat=sifrat*text%n;
+    sifrat=sifrat % n;
+  }
+  return sifrat;
+}
+
+//Dekriptiranje
+int decrypt(int sifrat, int n, int d){
+  int desifrat=1;
+  for(int i=0; i<d;i++){
+    desifrat=desifrat*sifrat%n;
+    desifrat=desifrat%n;
+  }
+  return desifrat;
+}
+
+
+//Odaberite dva velika prirodna broja
+int p=17;
+int q=23;
+
+int s;
+
+void setup() {
+  
+Serial.begin(9600);
+
+int n=p*q;
+int FIn=(p-1)*(q-1);
+
+//Odaberite "e" prirodni broj manji od FIn
+int e=35;
+
+//Računamo "d"
+int d=1;
+do{
+  s=(d*e)%FIn;
+  d++;
+}while(s!=1);
+d=d-1;
+
+String rijec = "Dorian i Goran";
+
+
+Serial.println("");
+Serial.print("Izvorni tekst: ");
+Serial.println(rijec);
+
+//Ispis ključeva
+Serial.print("Javni kljuc: {");
+Serial.print(e);
+Serial.print(",");
+Serial.print(n);
+Serial.print("}");
+
+Serial.println("");
+Serial.print("Tajni kljuc: {");
+Serial.print(d);
+Serial.print(",");
+Serial.print(n);
+Serial.println("}");
+
+int duljinaR=rijec.length();
+
+String sifra="";
+
+//Kriptiranje
+Serial.print("Kriptirani tekst: ");
+for (int i=0; i<duljinaR;i++){
+  char c = encrypt (rijec[i],n,e);
+  sifra=sifra+c;
+}
+Serial.println(sifra);
+
+String dsifra="";
+
+//Dekriptiranje
+int duljinaS=sifra.length();
+Serial.print("Dekriptirani tekst: ");
+for (int i=0; i<duljinaS; i++){
+  char c = decrypt (sifra[i],n,d);
+  dsifra=dsifra+c;
+}
+Serial.println(dsifra);
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+}
